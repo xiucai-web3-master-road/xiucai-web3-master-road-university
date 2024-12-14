97ece93ecd61aceea1aa029fe41a0eafe0e4d6c3
xiucai <865862724@qq.com>
2024-12-14 00:25:31 +0000

    lp mint success next mulipt chain develop

--- wagmi-dapp/src/app/page.tsx
+++ wagmi-dapp/src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import { useReadContract, useWriteContract } from "wagmi";
+import { useReadContract, useWriteContract,useWatchContractEvent } from "wagmi";
 import { parseEther } from "viem";
+import { message } from 'antd'
 
 function App() {
   const account = useAccount()
@@ -53,31 +54,63 @@ function App() {
     })
   }
 
+  useWatchContractEvent({
+    address: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
+    abi: [
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: false,
+            internalType: "address",
+            name: "minter",
+            type: "address",
+          },
+          {
+            indexed: false,
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256",
+          },
+        ],
+        name: "Minted",
+        type: "event",
+      },
+    ],
+    eventName: "Minted",
+    onLogs() {
+      message.success("LP铸造 Mint Token 成功!");
+      // 刷新页面
+      window.location.reload();
+    },
+  });
+
   return (
     <>
       <div>
-        <h1>秀才web3大师之路-60天企业级入行项目实战-前端开发教程</h1>
+        <h1>秀才web3大师之路-60天企业级入行项目实战</h1>
+
+        <h1 style={{ color: 'red' }}  >秀才web3大师之路-加入流程</h1>
 
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '20px', margin: '20px 0' }}>
           <div>
-            <h2>关注秀才-微信视频号</h2>
+            <h2 style={{ color: 'red' }} >1-关注秀才-微信视频号</h2>
             <img src="/images/微信视频号.png" alt="关注秀才-微信视频号" width={400} height={400}/>
           </div>
 
           <div>
-            <h2>关注秀才-微信号</h2>
+            <h2 style={{ color: 'red' }} >2-关注秀才-微信号</h2>
             <img src="/images/微信号.png" alt="关注秀才-微信号" width={400} height={400}/>
           </div>
           
           <div>
-            <h2>开通web3大师之路-知识星球-vip权限</h2>
+            <h2 style={{ color: 'red' }} >3-开通web3大师之路-知识星球-vip权限</h2>
             <img src="/images/知识星球.png" alt="开通web3大师之路-知识星球-vip权限" width={400} height={400} />
           </div>
         </div>
 
-       
 
-        <h2>Account</h2>
+        <h2 style={{ color: 'red' }} >我的钱包</h2>
 
         <div>
           status: {account.status}
@@ -97,7 +130,7 @@ function App() {
       </div>
 
       <div>
-        <h2>Connect</h2>
+        <h2 style={{ color: 'red' }} >钱包连接器</h2>
         {connectors.map((connector) => (
           <button
             key={connector.uid}
@@ -113,16 +146,15 @@ function App() {
 
       <div>
         <div>
-          <h2>NFT收藏品， 游戏道具，游戏币，LP铸造 Mint Token</h2>
+          <h2 style={{ color: 'red' }} >NFT收藏品， 游戏道具，游戏币，LP铸造 Mint Token</h2>
+            <button 
+            onClick={handleMint}
+            disabled={writeStatus === 'pending' || !account.address}
+          >
+            {writeStatus === 'pending' ? 'Minting...' : 'LP铸造 Mint Token'}
+          </button>
           <img src="/images/NFT收藏品.png" alt="NFT收藏品， 游戏道具，游戏币，LP铸造 Mint Token" width={400} height={400} />
         </div>
-       
-        <button 
-          onClick={handleMint}
-          disabled={writeStatus === 'pending' || !account.address}
-        >
-          {writeStatus === 'pending' ? 'Minting...' : 'Mint 1 Token'}
-        </button>
         
         {writeStatus === 'success' && <div style={{ color: 'green' }}>Mint 成功!</div>}
         {writeError && <div style={{ color: 'red' }}>Mint 失败，请重试</div>}
